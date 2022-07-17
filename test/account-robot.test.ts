ed71cd8653728826a978f56df8e9e7f32a9a7e48
Andrew Frazer <andrew.frazer@contactenergy.co.nz>
2022-07-17 20:01:07 +1200

    Initial commit

---
+++ test/account-robot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AccountRobot from '../lib/account-robot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/account-robot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AccountRobot.AccountRobotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
